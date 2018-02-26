4bef659cd878225a71ff538d8733b9d6fb660fcb
namnw <weerat.l@mail.kmutt.ac.th>
2018-02-26 22:32:22 +0700

    -start project

---
+++ jewery/src/main/java/com/jewery/JeweryApplication.java
@@ -0,0 +1,12 @@
+package com.jewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeweryApplication.class, args);
+	}
+}
